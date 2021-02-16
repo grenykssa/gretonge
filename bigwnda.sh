34c7b489dd80bb083f7b216ff74af4214766c57e
grenykssa <79082904+grenykssa@users.noreply.github.com>
2021-02-16 11:12:17 +0700

    Create bigwnda.sh

---
+++ bigwnda.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+green='\33[32;1m'
+blue='\33[0;36m'
+red='\33[31;1m'
+purple='\e[0;35m'
+Oren='\33[33;1m'
+black='\33[30;1m'
+white='\33[37;1m'
+yellow='\33[33;1m'
+
+clear
+
+toilet -f big -F gay fauzan ck
+echo
+sleep 2
+clear
+
+echo $Oren" 
+    =======================================
+    SELAMAT DATANG DI TOOLS FAUZAN PRATAMA
+    =======================================
+" | lolcat
+sleep 2
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay  password
+echo "\033[33;1m+=============================+"
+echo "\033[33;1mSILAKAN MASUKAN PW :fauzanpratama:"
+echo "\033[33;1mTelegram : t.me/xzuan"
+echo "\033[33;1mWhatsapp : 082217836017"
+echo "\033[33;1m+=============================+"
+      
+   name="fauzanpratama"
+while [ "$adin" != "$name" ]
+do
+  read -p "MASUKAN PASSWORD BRO:)" adin
+done
+  echo "OK PASSWORD ANDA BENAR..."
+  sleep 2
+  clear   
+  
+  blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay welcome
+
+echo "\033[32;1m    =============================="
+echo "\033[32;1m         AUTHOR : FAUZAN PRATAMA"
+echo "\033[32;1m         KONTAK : 081276754581"
+echo "\033[32;1m    ==============================="
+echo "\033[33;1m    ============MENU=============="
+echo "\033[33;1m     (01) BUG TSEL WHATSAPP"
+echo "\033[33;1m     (02) BUG TSEL INSTAGRAM"
+echo "\033[33;1m     (03) BUG TSEL YOUTUBE"
+echo "\033[33;1m     (04) BUG TSEL UNLIMAX"
+echo "\033[33;1m     (05) BUG TSEL KUOTA KB BIASA"
+echo "\033[33;1m     (06) BUG AXIS EDUKASI V2RAY"
+echo "\033[33;1m     (07) EXIT..."
+echo "\033[33;1m    ==============================="
+echo "\033[37;1m  <<<PILIH MENU>>>"
+read -p "  [ PILIH NOMOR ]>>>)" pilih ;
+if [ $pilih = "01" ]
+then
+clear
+toilet -f big -F gay bug wa
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ================"
+echo "\033[31;1m www.whatsapp.com"
+echo "\033[31;1m ================"
+sleep 2
+echo "\033[37;1m ++++BUG KUOTA WA++++"
+fi
+if [ $pilih = "02" ]
+then
+clear
+toilet -f big -F gay bug ig
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m =============="
+echo "\033[31;1m m.instagram.com"
+echo "\033[31;1m =============="
+sleep 2
+echo "\033[37;1m ++++BUG KUOTA IG++++"
+fi
+if [ $pilih = "03" ]
+then
+clear
+toilet -f big -F gay bug yt
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ==============="
+echo "\033[31;1m www.youtube.com"
+echo "\033[31;1m ==============="
+sleep 2
+echo "\033[37;1m ++++BUG KUOTA YOUTUBE++++"
+fi
+if [ $pilih = "04" ]
+then
+clear
+toilet -f big -F gay bug unli
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ================="
+echo "\033[31;1m  v.snr.whatsapp.net "
+echo "\033[31;1m  www.instagram.com"
+echo "\033[31;1m  ================="
+sleep 2
+echo "\033[37;1m ++++BUG KUOTA TSEL UNLIMAX++++"
+fi
+if [ $pilih = "05" ]
+then
+clear
+toilet -f big -F gay bug kb
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ==========="
+echo "\033[31;1m www.zoom.us"
+echo "\033[31;1m ==========="
+sleep 2
+echo "\033[37;1m ++++BUG KUOTA BELAJAR TSEL++++"
+fi
+if [ $pilih = "06" ]
+then
+clear
+toilet -f big -F gay bug axis edu
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m =================="
+echo "\033[31;1m bimbel.ruangguru.com"
+echo "\033[31;1m =================="
+sleep 2
+echo "\033[37;1m ++++BUG KUOTA V2RAY EDU AXIS++++"
+fi
+elif [ $pil = "07" ]
+    then
+      echo "LAGI KELUAR NGAB..."
+      sleep 2
+      exit
+    else
+      echo "MODULE SALAH BEGO"
+      sleep 2
+      echo $ulang
+    fi
+done
